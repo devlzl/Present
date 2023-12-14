42d94ef63d95a070efb1f152a64bc09f189919af
Zhilin Liu <zhilin7@qq.com>
2023-12-14 19:49:13 +0800

    init: Hello Present

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello Present !" />
+</template>
+
+<style scoped></style>
