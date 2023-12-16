e54e7ebc5ec0cb1ed69d8fa9de3b41f18a22b095
SkyeSun <try_sun@outlook.com>
2023-12-16 21:36:21 +0800

    chore: i18n

---
+++ src/i18n/Test/index.ts
@@ -0,0 +1,8 @@
+export default {
+  zh: {
+    msg: '我是一句中文。'
+  },
+  en: {
+    msg: 'This is English.'
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import { messages } from './i18n'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  locale: 'zh',
+  fallbackLocale: 'en',
+  messages,
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.mount('#app')
