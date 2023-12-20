eeb92aee3ed15efe24b554047f264c492e7e62dd
Zhilin Liu <zhilin7@qq.com>
2023-12-20 20:50:40 +0800

    fix: EventManager parameter type (#8)

--- src/Kernel/EventManager.ts
+++ src/Kernel/EventManager.ts
@@ -1,15 +1,17 @@
-interface EventCalback<T> {
-  (data?: T): void
+interface EventCallback<T> {
+  (data: T): void
 }
 
 export class EventManager<T> {
-  private _callbacks: Array<EventCalback<T>> = []
+  private _callbacks: Array<EventCallback<T>> = []
 
-  on(callback: EventCalback<T>) {
+  on(callback: EventCallback<T>) {
     this._callbacks.push(callback)
   }
 
   emit(data?: T) {
-    this._callbacks.forEach((callback) => callback(data))
+    // data?: T           // make the `data` optional
+    // as NonNullable<T>  // no need to check if `data` is undefined in the callback
+    this._callbacks.forEach((callback) => callback(data as NonNullable<T>))
   }
 }
