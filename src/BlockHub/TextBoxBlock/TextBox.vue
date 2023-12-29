894f5f87aff12c3d66b37a2ea92e76df16cc6fa4
Zhilin Liu <zhilin7@qq.com>
2023-12-29 17:34:58 +0800

    feat: RichText format api

--- src/BlockHub/TextBoxBlock/TextBox.vue
+++ src/BlockHub/TextBoxBlock/TextBox.vue
@@ -6,12 +6,16 @@ const { block } = defineProps<{
   block: TextBoxBlock
 }>()
 
-const { x, y, width, height, text } = block
+const { x, y, width, height, textStore, bindController } = block
+
+function format(name: string, value: any) {
+  block.controller?.format(name, value)
+}
 </script>
 
 <template>
   <div
-    class="absolute border border-dashed border-secondary-border"
+    class="text-box absolute border border-dashed border-secondary-border"
     :style="{
       left: `${x}px`,
       top: `${y}px`,
@@ -19,6 +23,29 @@ const { x, y, width, height, text } = block
       height: `${height}px`,
     }"
   >
-    <RichText :textStore="text" />
+    <button class="border border-primary rounded-sm text-primary bg-white hover:bg-primary hover:text-white m-2 px-2" @click="format('bold', true)">
+      bold
+    </button>
+    <button class="border border-primary rounded-sm text-primary bg-white hover:bg-primary hover:text-white m-2 px-2" @click="format('italic', true)">
+      italic
+    </button>
+    <button
+      class="border border-primary rounded-sm text-primary bg-white hover:bg-primary hover:text-white m-2 px-2"
+      @click="format('underline', true)"
+    >
+      underline
+    </button>
+    <button class="border border-primary rounded-sm text-primary bg-white hover:bg-primary hover:text-white m-2 px-2" @click="format('strike', true)">
+      strike
+    </button>
+    <select @change="(event) => format('fontSize', (event.target as HTMLSelectElement).value)">
+      <option v-for="option in [14, 20, 30, 50, 100]" :value="option">
+        {{ option }}
+      </option>
+    </select>
+    <input type="color" @input="(event) => format('color', (event.target as HTMLInputElement).value)" />
+    <input type="color" @input="(event) => format('background', (event.target as HTMLInputElement).value)" />
+
+    <RichText :textStore="textStore" :bindController="bindController" />
   </div>
 </template>
--- src/RichText/RichText.ts
+++ src/RichText/RichText.ts
@@ -2,6 +2,10 @@ import { type TextStore } from '@Kernel/Store/TextStore'
 import { SelectionHandler } from './handler/SelectionHandler'
 import { EventHandler } from './handler/EventHandler'
 
+export interface RichTextController {
+  format(name: string, value: any): void
+}
+
 export class RichText {
   element?: HTMLElement
   textStore: TextStore
@@ -19,5 +23,16 @@ export class RichText {
   mount(element: HTMLElement) {
     this.element = element
     this.eventHandler.mount()
+  }
+
+  get controller(): RichTextController {
+    return {
+      format: (name: string, value: any) => {
+        const { index, length } = this.getSelection()
+        this.textStore.format(index, length, {
+          [name]: value,
+        })
+      },
+    }
   }
 }
