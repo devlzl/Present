7f96ded497fee7dd9f8e325dbc23ff4d8a0e31b0
Zhilin Liu <zhilin7@qq.com>
2024-01-10 14:35:39 +0800

    feat: PictureBlock url prop (#47)

--- src/BlockHub/PictureBlock/PictureBlock.ts
+++ src/BlockHub/PictureBlock/PictureBlock.ts
@@ -4,4 +4,12 @@ export class PictureBlock extends Block {
   constructor(x: number, y: number) {
     super('Picture', x, y, 200, 200)
   }
+
+  get url() {
+    return this.props.get('url') as string
+  }
+
+  set url(url: string) {
+    this.props.set('url', url)
+  }
 }
