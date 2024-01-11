229392b8767cabc94ef198255b3ee6636773ed63
Zhilin Liu <zhilin7@qq.com>
2024-01-11 11:42:43 +0800

    feat: negative direction drag generate TextBox

--- src/Kernel/ToolBox/ToolBox.ts
+++ src/Kernel/ToolBox/ToolBox.ts
@@ -21,32 +21,37 @@ export class ToolBox {
   }
 
   private _bindEvents() {
-    const eventNames = ['Click', 'MouseDown', 'MouseMove', 'MouseUp'] as const
     const previousHandlerMap = Object.create(null)
+
     const _addListener = () => {
-      eventNames.forEach((eventName) => {
-        const handler = this._currentController[`handle${eventName}`].bind(this._currentController)
-        if (eventName === 'MouseUp') {
-          document.addEventListener('mouseup', handler)
-        } else {
-          this._slideElement?.addEventListener(
-            eventName.toLowerCase() as Lowercase<(typeof eventNames)[number]>,
-            handler
-          )
-        }
-        previousHandlerMap[eventName] = handler
-      })
+      const controller = this._currentController
+      const slideElement = this._slideElement as HTMLElement
+
+      const handleClick = controller.handleClick.bind(this._currentController)
+      slideElement.addEventListener('click', handleClick)
+      previousHandlerMap.click = handleClick
+
+      const handleMouseDown = controller.handleMouseDown.bind(this._currentController)
+      slideElement.addEventListener('mousedown', handleMouseDown)
+      previousHandlerMap.mousedown = handleMouseDown
+
+      const handleMouseMove = controller.handleMouseMove.bind(this._currentController)
+      slideElement.addEventListener('mousemove', handleMouseMove)
+      previousHandlerMap.mousemove = handleMouseMove
+
+      const handleMouseUp = controller.handleMouseUp.bind(this._currentController)
+      document.addEventListener('mouseup', handleMouseUp)
+      previousHandlerMap.mouseup = handleMouseUp
     }
+
     const _removeListener = () => {
-      eventNames.forEach((eventName) => {
-        const handler = previousHandlerMap[eventName]
-        if (eventName === 'MouseUp') {
-          document.removeEventListener('mouseup', handler)
-        } else {
-          this._slideElement?.removeEventListener(eventName.toLowerCase(), previousHandlerMap[eventName])
-        }
-      })
+      const slideElement = this._slideElement as HTMLElement
+      slideElement.removeEventListener('click', previousHandlerMap.click)
+      slideElement.removeEventListener('mousedown', previousHandlerMap.mousedown)
+      slideElement.removeEventListener('mousemove', previousHandlerMap.mousemove)
+      document.removeEventListener('mouseup', previousHandlerMap.mouseup)
     }
+
     _addListener()
     this.events.toolChange.on((toolType) => {
       this._currentToolType = toolType
