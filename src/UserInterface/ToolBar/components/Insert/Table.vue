38db6ddb46127170032bfb6b032f1705a0fa1148
Zhilin Liu <zhilin7@qq.com>
2024-01-15 19:50:05 +0800

    feat: insert Table (#66)
    * feat:  add table create feature
    
    * refactor: use ToolButton
    
    * fix: click on gap
    
    * feat: update table size
    
    * chore: delete log
    
    ---------
    
    Co-authored-by: 曹俊俊 <1300722971@qq.com>

--- src/UserInterface/ToolBar/components/Insert/Table.vue
+++ src/UserInterface/ToolBar/components/Insert/Table.vue
@@ -1,23 +1,58 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { TableFile } from '@icon-park/vue-next'
 import ButtonGroup from '../ButtonGroup.vue'
-import { slideManager } from '@Kernel/index'
+import { selectionManager, slideManager } from '@Kernel/index'
 import { TableBlock } from '@BlockHub/TableBlock/TableBlock'
 import ToolButton from '../ToolButton.vue'
 
+const curRowIndex = ref<number>(0)
+const curColIndex = ref<number>(0)
+
 const insertTable = () => {
-  const block = new TableBlock(300, 200, 4, 3)
+  const block = new TableBlock(100, 100, curRowIndex.value, curColIndex.value)
   slideManager.currentSlide.addBlock(block)
+  selectionManager.focus(block)
+  curRowIndex.value = 0
+  curColIndex.value = 0
+}
+
+const mouseOverHandler = (rowIndex: number, colIndex: number) => {
+  curRowIndex.value = rowIndex
+  curColIndex.value = colIndex
+}
+
+const mouseLeaveHandler = () => {
+  curRowIndex.value = 0
+  curColIndex.value = 0
 }
 </script>
 
 <template>
   <ButtonGroup :name="$t('ToolBar.insert.table.title')">
-    <ToolButton :clickHandler="insertTable" :hasMenu="true">
+    <ToolButton :hasMenu="true">
       <template #icon>
         <TableFile class="mb-1" theme="outline" size="32" fill="#333" :strokeWidth="2" />
       </template>
       <template #name>{{ $t('ToolBar.insert.table.title') }}</template>
+      <template #menu>
+        <div class="px-[16px] pt-[6px] pb-[12px]">
+          <div class="h-[28px]">
+            {{ $t('ToolBar.insert.table.insert') }}
+          </div>
+          <div @click="insertTable" @mouseleave="mouseLeaveHandler" class="flex flex-wrap gap-[4px]">
+            <div v-for="rowIndex of 8" :key="rowIndex" class="flex justify-center gap-[4px]">
+              <div
+                v-for="colIndex of 10"
+                :key="`${rowIndex}-${colIndex}`"
+                class="w-[17px] h-[17px] border"
+                :class="rowIndex <= curRowIndex && colIndex <= curColIndex ? 'bg-blue-100 border-blue-400' : ''"
+                @mouseover="mouseOverHandler(rowIndex, colIndex)"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </template>
     </ToolButton>
   </ButtonGroup>
 </template>
