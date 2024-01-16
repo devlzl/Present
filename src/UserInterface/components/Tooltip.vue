e2696ad137f9726b0e783cc1f1d2e9737bf2a780
Zhilin Liu <zhilin7@qq.com>
2024-01-16 11:16:49 +0800

    feat: button Tooltip

---
+++ src/UserInterface/components/Tooltip.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { text } = defineProps<{
+  text: string
+}>()
+
+const showTooltip = ref(false)
+</script>
+
+<template>
+  <div class="relative" @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+    <slot></slot>
+    <div
+      v-if="showTooltip"
+      class="absolute bottom-[-24px] bg-secondary border px-[3px] rounded-[2px]"
+      :style="{ textWrap: 'nowrap' }"
+    >
+      {{ text }}
+    </div>
+  </div>
+</template>
