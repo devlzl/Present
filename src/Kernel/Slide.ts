a1235d9a8275ec19648b897b6da2c4d20edcb398
Zhilin Liu <zhilin7@qq.com>
2024-01-31 11:04:35 +0800

    feat: history for add and remove block (#82)

--- src/Kernel/Slide.ts
+++ src/Kernel/Slide.ts
@@ -1,5 +1,7 @@
 import { type Block } from '@BlockHub/Block/Block'
 import { EventManager } from './EventManager'
+import { Command } from '@Kernel/HistoryManager'
+import { history } from '@Kernel/index'
 
 export class Slide {
   static id = 0
@@ -24,13 +26,33 @@ export class Slide {
     return this._blocks.slice()
   }
 
-  addBlock(block: Block) {
+  private _addBlock(block: Block) {
     this._blocks.push(block)
     this.events.blockChange.emit()
   }
 
-  removeBlock(block: Block) {
-    this._blocks.splice(this._blocks.indexOf(block), 1)
+  private _removeBlock(block?: Block) {
+    if (block) {
+      this._blocks.splice(this._blocks.indexOf(block), 1)
+    } else {
+      this._blocks.pop()
+    }
     this.events.blockChange.emit()
+  }
+
+  addBlock(block: Block) {
+    const command = new Command(
+      () => this._addBlock(block),
+      () => this._removeBlock(),
+    )
+    history.exec(command)
+  }
+
+  removeBlock(block: Block) {
+    const command = new Command(
+      () => this._removeBlock(block),
+      () => this._addBlock(block)
+    )
+    history.exec(command)
   }
 }
