dd67271664eb3218d3888e648930900e824693c1
Zhilin Liu <zhilin7@qq.com>
2024-01-31 21:09:41 +0800

    feat: zoom

---
+++ src/Kernel/Zoom.ts
@@ -0,0 +1,18 @@
+import { EventManager } from './EventManager'
+
+export class Zoom {
+  private _zoom = 1
+  private _changeByHandle = false
+
+  updateEvent = new EventManager<number>()
+
+  get changeByHandle() {
+    return this._changeByHandle
+  }
+
+  update(value: number, handle: boolean = false) {
+    this._zoom = value
+    this._changeByHandle = handle
+    this.updateEvent.emit(this._zoom)
+  }
+}
--- src/UserInterface/StatusBar/StatusBar.vue
+++ src/UserInterface/StatusBar/StatusBar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import LocaleChanger from './LocaleChanger.vue'
+import LocaleChanger from './components/LocaleChanger.vue'
+import Zoom from './components/Zoom.vue'
 </script>
 
 <template>
-  <div class="h-[24px] bg-secondary border-t border-secondary-hover">
+  <div class="flex justify-between h-[24px] bg-secondary border-t border-secondary-hover">
     <LocaleChanger />
+    <Zoom />
   </div>
 </template>
